d550a2f5647cbd35dbe1066b722911706d072bee
Catharina Mesquita <catha.ana.1994@gmail.com>
2018-02-18 10:32:02 -0300

    Adding function Todo#handleAdd in button component

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -5,11 +5,13 @@ import Button from '../template/button'
 
 export default props => (
     <div role='form' className='todoForm'>
-        <Grid cols="12 9 10">
+        <Grid cols='12 9 10'>
             <input id='description' className='form-control' placeholder='Adicione ou busque um tarefa' />
         </Grid>
-        <Grid cols="12 3 2">
-            <Button style="primary" icon="plus"></Button>
+        <Grid cols='12 3 2'>
+            <Button style="primary" icon="plus"
+                onClick={props.handleAdd}
+            ></Button>
         </Grid>
     </div>
 )
