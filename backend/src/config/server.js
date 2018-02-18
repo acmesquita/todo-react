b584597fe5f209ceaf8a832184b8ac0e8851f03d
Catharina Mesquita <catha.ana.1994@gmail.com>
2018-02-18 11:21:53 -0300

    Improving Cors dependences

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,13 +1,15 @@
 const port = 3003
 
-const allowCors = require('./cors')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
-server.use(allowCors)
+
+server.use(cors({origin: '*'}));
+server.set('secret', 'opensecret');
 
 server.listen(port, function () {
     console.log(`BACKEND is running on port ${port}.`);    
