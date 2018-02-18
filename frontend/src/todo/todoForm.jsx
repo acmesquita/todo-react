916b8412a4d44c7592825cd72ebd5f1849724b9e
Catharina Mesquita <catha.ana.1994@gmail.com>
2018-02-18 14:51:47 -0300

    Adding keyboards commands

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -3,12 +3,23 @@ import React from 'react'
 import Grid from '../template/grid'
 import Button from '../template/button'
 
-export default props => (
-    <div role='form' className='todoForm'>
+export default props => {
+    
+    const keyHandler = (e)=>{
+        if (e.key === 'Enter'){
+            e.shiftKey ? props.handleSearch() : props.handleAdd()
+        }else if (e.key === 'Escape'){
+            props.handleClear()
+        }
+    }
+    
+    return (
+        <div role='form' className='todoForm'>
         <Grid cols='12 9 10'>
             <input id='description' className='form-control' 
             placeholder='Adicione ou busque um tarefa' 
             onChange={props.handleChange}
+            onKeyUp={keyHandler}
             value={props.description}
             />
         </Grid>
@@ -25,4 +36,5 @@ export default props => (
         </Grid>
         
     </div>
-)
+    )
+}
