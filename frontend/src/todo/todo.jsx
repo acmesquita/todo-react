4c050f033592e37b0962ccc380162b3bf2311041
Catharina Mesquita <catha.ana.1994@gmail.com>
2018-02-18 10:48:08 -0300

    Improving integration with Input and Button add

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -7,18 +7,27 @@ export default class Todo extends Component{
 
     constructor(props){
         super(props)
+        this.state = { description: '', list: [] }
         this.handleAdd = this.handleAdd.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(event){
+        this.setState({ ...this.state, description: event.target.value })
     }
 
     handleAdd(){
-        console.log(this);
+       console.log(this.state.description);
     }
 
     render(){
         return (
             <div>
                 <PageHeard name='Tarefas' small='Cadastro'/>
-                <TodoForm handleAdd={this.handleAdd}/>
+                <TodoForm 
+                handleAdd={this.handleAdd} 
+                handleChange={this.handleChange} 
+                description={this.state.description}/>
                 <hr/>
                 <TodoList />
             </div>
--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -6,7 +6,11 @@ import Button from '../template/button'
 export default props => (
     <div role='form' className='todoForm'>
         <Grid cols='12 9 10'>
-            <input id='description' className='form-control' placeholder='Adicione ou busque um tarefa' />
+            <input id='description' className='form-control' 
+            placeholder='Adicione ou busque um tarefa' 
+            onChange={props.handleChange}
+            value={props.description}
+            />
         </Grid>
         <Grid cols='12 3 2'>
             <Button style="primary" icon="plus"
