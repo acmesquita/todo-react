f863492aecc135bc0d0b3853e91bcd2fb99a7a08
Catharina Mesquita <catha.ana.1994@gmail.com>
2018-02-17 21:31:55 -0300

    improving form

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
 export default props => (
-    <div>
-        <h2>Form</h2>
+    <div role='form' className='todoForm'>
+        <div className='col-xs-12 col-sm-9 col-md-10'>
+            <input id='description' className='form-control' placeholder='Adicione ou busque um tarefa' />
+        </div>
+        <div className='col-xs-12 col-sm-3 col-md-2'>
+            <button id='add' className='btn btn-primary'>
+                <i className="fa fa-plus"></i>
+            </button>
+        </div>
     </div>
 )
